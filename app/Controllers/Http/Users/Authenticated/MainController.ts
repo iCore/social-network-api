9a0c9ac8e14374eb7a97390da94c1eba008ed671
Jocian Mendonça <jocianmendonca@gmail.com>
2022-05-29 14:38:15 +0100

    feat: added the user exclusion route

--- app/Controllers/Http/Users/Authenticated/MainController.ts
+++ app/Controllers/Http/Users/Authenticated/MainController.ts
@@ -1,3 +1,4 @@
+import Drive from '@ioc:Adonis/Core/Drive'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { User } from 'App/Models'
 import UpdateValidator from 'App/Validators/Users/Authenticated/Main/UpdateValidator'
@@ -18,5 +19,17 @@ export default class MainController {
 
     await User.updateOrCreate({ id: user.id }, { username, password })
     await user.related('profile').updateOrCreate({}, { ...profile })
+  }
+
+  public async destroy({ auth }: HttpContextContract) {
+    const user = auth.user!
+
+    await auth.logout()
+
+    await user.load('profile')
+
+    if (await Drive.exists(user.profile.avatar)) await Drive.delete(user.profile.avatar)
+
+    await user.delete()
   }
 }
